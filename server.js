8e6f3957a22fa964cdd9510971e2eab6d1ce5d11
Bryce Thompson <bryceedthompson@gmail.com>
2023-06-20 16:40:47 -0700

    i think it works

--- server.js
+++ server.js
@@ -1,32 +1,54 @@
 const express = require('express');
-
 const path = require('path');
-
-const api = require('./routes/notes');
+const uuid = require('uuid');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-
+//express
 app.use(express.json());
-
 app.use(express.urlencoded({extended: true}));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
+//notes path
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
 
-app.use('/api/notes',api);
+  // api path
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
+  });
 
-app.use(express.static('public'));
+  // Html path
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
+  // post path
+  app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      let db = JSON.parse(data);
+      db.push({
+        note_id: uuid.v4(),
+        ...req.body,
+      });
+      fs.writeFile(
+        path.join(__dirname,  "./db/db.json"),
+        JSON.stringify(db, null, 2),
+        (err, data) => {
+          if (err) throw err;
+          res.json(db);
+        }
+      );
+    });
+  });
 
-app.use('/',(req,res) =>
-res.sendFile(path.join(__dirname,'/public/index.html'))
-);
-
-app.use('/',(req,res) =>
-res.sendFile(path.join(__dirname,'/public/notes.html'))
-);
-
-app.listen(PORT,() => 
-console.log('app listening at http://localhost:${PORT}')
+  //link through terminal
+  app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
